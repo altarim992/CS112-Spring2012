06bd154fbfdaa0e9c25dc11c83f7e8a3ff05dd68
Matt Pink <mp11@stout.hampshire.edu>
2012-02-05 21:29:29 -0500

    hw03 done

--- hw03/prissybot.py
+++ hw03/prissybot.py
@@ -46,3 +46,42 @@ Prissy bot, the rude chat bot, is just mean!  It does not listen, asks obnoxious
 #     * read a numerator and denominator from the user
 #     * ex.  6/4 = 1 2/4
 
+#Defining variables
+p="Prissybot: "
+a=raw_input("Enter your name: ")
+b=a+": "
+n=3/2
+m=3+4
+print p,"Hello there,",a
+
+c=raw_input(b)
+
+print p,"You mean "+"'"+c+", sir!'"
+print p,"Math is fun! Don't you agree?"
+
+raw_input(b)
+
+print p,"I'm not really interested in your opinion,",a+"."
+print p,"What is 3 divided by 2?"
+
+raw_input(b)
+
+print p,"No, stupid. 3/2 is obviously equal to",n
+print p,"Maybe your clearly inferior human brain needs something simpler. How about 3+4?"
+
+raw_input(b)
+
+print p,"3+4 is",m
+print p,"Well you'll never be able to compete with my intellect. Try me. I'm going to ask you for 3 numbers, which I will multiply together."
+
+d=raw_input("Enter the year you were born: ")
+d=int(d)
+e=raw_input("Enter your favorite number (decimals make it even more fun!): ")
+e=float(e)
+
+print p,d,"*",e,"=",d*e, "That was easier than your mother."
+
+f=raw_input(b)
+
+print p,"First, that's '"+f+",' sir! Second, talking to you has been utterly mundane. Goodbye."
+print "Application 'prissybot.py' terminated."
