dfb78db31709a3f0cdb7c8e4790a8458a32bf026
Matt Pink <mp11@stout.hampshire.edu>
2012-02-05 21:30:38 -0500

    hw03 done

---
+++ hw03/ascii.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+char=raw_input("Enter a character: ")
+s= " "
+
+print s * 7, char * 2
+print s * 7, char * 2
+print s * 5, char * 6
+print s * 2, char * 12
+print s * 1, char * 16
+print s * 3, char * 5
+print s * 5, char * 5
+print s * 7, char * 5
+print s * 9, char * 5
+print s * 1, char * 16
+print s * 2, char * 12
+print s * 5, char * 6
+print s * 7, char * 2
+print s * 7, char * 2
